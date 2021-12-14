54a69a8aa80a8a49f9b9d48b2e22f02587c0f0ef
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-14 19:11:58 +0530

    updated

---
+++ assignmentoperators.js
@@ -0,0 +1,21 @@
+var x=10;
+var y=15;
+var z=20;
+
+x += 15
+console.log(x)
+
+y -= 5
+console.log(y)
+
+z *= 2
+console.log(z)
+
+x /= 5
+console.log(x)
+
+y %= 3
+console.log(y)
+
+z <<= 3
+console.log(z)
